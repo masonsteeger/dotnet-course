f808f0302ff55d6021641b6a9d287ddbf2659b6a
Mason Steeger <68522133+masonsteeger@users.noreply.github.com>
2023-05-12 14:32:59 -0500

    End of section 13

--- client-app/src/app/common/form/MyDateInput.tsx
+++ client-app/src/app/common/form/MyDateInput.tsx
@@ -1,22 +1,21 @@
-import { useField } from "formik";
-import ReactDatePicker, { ReactDatePickerProps } from "react-datepicker";
-import { Form, Label } from "semantic-ui-react";
+import React from 'react';
+import {useField} from "formik";
+import {Form, Label} from "semantic-ui-react";
+import DatePicker, {ReactDatePickerProps} from 'react-datepicker';
 
-export default function MyTextInput(props: Partial<ReactDatePickerProps>) {
-  const [field, meta, helpers] = useField(props.name!);
-  return (
-    <Form.Field error={meta.touched && !!meta.error}>
-      <ReactDatePicker
-        {...field}
-        {...props}
-        selected={field.value && new Date(field.value || null)}
-        onChange={(value) => helpers.setValue(value)}
-      />
-      {meta.touched && meta.error ? (
-        <Label basic color='red'>
-          {meta.error}
-        </Label>
-      ) : null}
-    </Form.Field>
-  );
+export default function MyDateInput(props: Partial<ReactDatePickerProps>) {
+    const [field, meta, helpers] = useField(props.name!);
+    return (
+        <Form.Field error={meta.touched && !!meta.error}>
+            <DatePicker
+                {...field}
+                {...props}
+                selected={(field.value && new Date(field.value)) || null}
+                onChange={value => helpers.setValue(value)}
+            />
+            {meta.touched && meta.error ? (
+                <Label basic color='red'>{meta.error}</Label>
+            ) : null}
+        </Form.Field>
+    )
 }
