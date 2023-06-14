063c56a4428704806c28d7d04669743d12b1e5c4
Mason Steeger <68522133+masonsteeger@users.noreply.github.com>
2023-06-14 13:58:12 -0500

    end of section 19

---
+++ client-app/src/app/models/comment.ts
@@ -0,0 +1,8 @@
+export interface ChatComment {
+  id: number;
+  createdAt: any;
+  username: string;
+  displayName: string;
+  image: string;
+  body: string;
+}
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,7 +1,7 @@
 import { Grid } from "semantic-ui-react";
-import { useStore } from '../../../app/stores/store';
-import { observer } from 'mobx-react-lite';
-import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import ActivityDetailedChat from "./ActivityDetailedChat";
@@ -10,26 +10,32 @@ import ActivityDetailedInfo from "./ActivityDetailedInfo";
 import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
 
 export default observer(function ActivityDetails() {
-    const { activityStore } = useStore();
-    const { selectedActivity: activity, loadActivity, loadingInitial } = activityStore;
-    const { id } = useParams();
+  const { activityStore } = useStore();
+  const {
+    selectedActivity: activity,
+    loadActivity,
+    loadingInitial,
+    clearSelectedActivity,
+  } = activityStore;
+  const { id } = useParams();
 
-    useEffect(() => {
-        if (id) loadActivity(id);
-    }, [id, loadActivity]);
+  useEffect(() => {
+    if (id) loadActivity(id);
+    return () => clearSelectedActivity();
+  }, [id, loadActivity, clearSelectedActivity]);
 
-    if (loadingInitial || !activity) return <LoadingComponent />
+  if (loadingInitial || !activity) return <LoadingComponent />;
 
-    return (
-        <Grid>
-            <Grid.Column width='10'>
-                <ActivityDetailedHeader activity={activity} />
-                <ActivityDetailedInfo activity={activity} />
-                <ActivityDetailedChat />
-            </Grid.Column>
-            <Grid.Column width='6'>
-                <ActivityDetailedSidebar activity={activity}/>
-            </Grid.Column>
-        </Grid>
-    )
-})
+  return (
+    <Grid>
+      <Grid.Column width='10'>
+        <ActivityDetailedHeader activity={activity} />
+        <ActivityDetailedInfo activity={activity} />
+        <ActivityDetailedChat activityId={activity.id} />
+      </Grid.Column>
+      <Grid.Column width='6'>
+        <ActivityDetailedSidebar activity={activity} />
+      </Grid.Column>
+    </Grid>
+  );
+});
