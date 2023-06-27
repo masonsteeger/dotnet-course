7bf4ab8e028e034c4244c668da943ee4911b8fff
Mason Steeger <68522133+masonsteeger@users.noreply.github.com>
2023-06-27 16:54:19 -0500

    finished refresh tokens

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -37,7 +37,7 @@ axios.interceptors.response.use(
     return response;
   },
   (error: AxiosError) => {
-    const { data, status, config } = error.response as AxiosResponse;
+    const { data, status, config, headers } = error.response as AxiosResponse;
     switch (status) {
       case 400:
         if (config.method === "get" && data.errors.hasOwnProperty("id")) {
@@ -56,10 +56,18 @@ axios.interceptors.response.use(
         }
         break;
       case 401:
-        toast.error("unauthorised");
+        if (
+          status === 401 &&
+          headers["www-authenticate"]?.startsWith(
+            'Bearer error="invalid_token"'
+          )
+        ) {
+          store.userStore.logout();
+          toast.error("Session Expired - please login again");
+        }
         break;
       case 403:
-        toast.error("forbidden");
+        toast.error("Forbidden");
         break;
       case 404:
         router.navigate("/not-found");
@@ -102,6 +110,7 @@ const Account = {
     requests.post<User>("/account/register", user),
   fbLogin: (accessToken: string) =>
     requests.post<User>(`/account/fbLogin?accessToken=${accessToken}`, {}),
+  refreshToken: () => requests.post<User>("/account/refreshToken", {}),
 };
 
 const Profiles = {
--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -7,6 +7,8 @@ import { store } from "./store";
 export default class UserStore {
   user: User | null = null;
   fbLoading = false;
+  refreshTokenTimeout: any;
+
   constructor() {
     makeAutoObservable(this);
   }
@@ -19,6 +21,7 @@ export default class UserStore {
     try {
       const user = await agent.Account.login(creds);
       store.commonStore.setToken(user.token);
+      this.startRefreshTokenTimer(user);
       runInAction(() => (this.user = user));
       router.navigate("/activities");
       store.modalStore.closeModal();
@@ -31,6 +34,7 @@ export default class UserStore {
     try {
       const user = await agent.Account.register(creds);
       store.commonStore.setToken(user.token);
+      this.startRefreshTokenTimer(user);
       runInAction(() => (this.user = user));
       router.navigate("/activities");
       store.modalStore.closeModal();
@@ -48,7 +52,9 @@ export default class UserStore {
   getUser = async () => {
     try {
       const user = await agent.Account.current();
+      store.commonStore.setToken(user.token);
       runInAction(() => (this.user = user));
+      this.startRefreshTokenTimer(user);
     } catch (error) {
       console.log(error);
     }
@@ -67,6 +73,7 @@ export default class UserStore {
       this.fbLoading = true;
       const user = await agent.Account.fbLogin(accessToken);
       store.commonStore.setToken(user.token);
+      this.startRefreshTokenTimer(user);
       runInAction(() => {
         this.user = user;
         this.fbLoading = false;
@@ -77,4 +84,27 @@ export default class UserStore {
       runInAction(() => (this.fbLoading = false));
     }
   };
+
+  refreshToken = async () => {
+    this.stopRefreshTokenTimer();
+    try {
+      const user = await agent.Account.refreshToken();
+      runInAction(() => (this.user = user));
+      store.commonStore.setToken(user.token);
+      this.startRefreshTokenTimer(user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  private startRefreshTokenTimer(user: User) {
+    const jwtToken = JSON.parse(atob(user.token.split(".")[1]));
+    const expires = new Date(jwtToken.exp * 1000);
+    const timeout = expires.getTime() - Date.now() - 30 * 1000;
+    this.refreshTokenTimeout = setTimeout(this.refreshToken, timeout);
+  }
+
+  private stopRefreshTokenTimer() {
+    clearTimeout(this.refreshTokenTimeout);
+  }
 }
